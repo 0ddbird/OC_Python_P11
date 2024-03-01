296123bdd34bbbda50aa133c3fb143d01d155f56
0ddbird <0ddbird@proton.me>
2024-03-01 11:05:38 +0100

    CHORE | Formatting, imports sorting, switched to snake case

--- server.py
+++ server.py
@@ -1,59 +1,73 @@
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+import os
 
+from dotenv import load_dotenv
 
-def loadClubs():
-    with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+from flask import Flask, flash, redirect, render_template, request, url_for
 
-
-def loadCompetitions():
-    with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
-
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'something_special'
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
-competitions = loadCompetitions()
-clubs = loadClubs()
 
-@app.route('/')
+def load_clubs():
+    with open("clubs.json") as c:
+        clubs_list = json.load(c)["clubs"]
+        return clubs_list
+
+
+def load_competitions():
+    with open("competitions.json") as comps:
+        competitions_list = json.load(comps)["competitions"]
+        return competitions_list
+
+
+competitions = load_competitions()
+clubs = load_clubs()
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
-
-@app.route('/showSummary',methods=['POST'])
-def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    return render_template("index.html")
 
 
-@app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+@app.route("/showSummary", methods=["POST"])
+def show_summary():
+    club = [club for club in clubs if club["email"] == request.form["email"]][0]
+    return render_template("welcome.html", club=club, competitions=competitions)
+
+
+@app.route("/book/<competition>/<club>")
+def book(competition, club):
+    found_club = [c for c in clubs if c["name"] == club][0]
+    found_competition = [c for c in competitions if c["name"] == competition][0]
+    if found_club and found_competition:
+        return render_template(
+            "booking.html", club=found_club, competition=found_competition
+        )
     else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        flash("Something went wrong, please try again.")
+        return render_template("welcome.html", club=club, competitions=competitions)
 
 
-@app.route('/purchasePlaces',methods=['POST'])
-def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+@app.route("/purchasePlaces", methods=["POST"])
+def purchase_places():
+    competition = [c for c in competitions if c["name"] == request.form["competition"]][
+        0
+    ]
+    club = [c for c in clubs if c["name"] == request.form["club"]][0]
+    required_places = int(request.form["places"])
+    competition["number_of_places"] = (
+        int(competition["number_of_places"]) - required_places
+    )
+    flash("Great! Booking complete!")
+    return render_template("welcome.html", club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
