add18d8afc23cb6a0934f6ccf53a74f629084bdd
0ddbird <0ddbird@proton.me>
2024-03-14 10:23:34 +0100

    FEAT | Grouped home route unit tests in a single class

--- tests/test_server_routes.py
+++ tests/test_server_routes.py
@@ -15,29 +15,29 @@ def client():
 def test_index_route(client):
     rv = client.get("/")
     assert rv.status_code == 200
+    # Check the response too (ex content in HTML -> assert title is in the response)
 
 
-def test_home_route_redirects_when_unauthenticated(client):
-    rv = client.get("/home")
-    assert rv.status_code == 302
+class TestHomeRoute:
+    def test_home_route_redirects_when_unauthenticated(self, client):
+        rv = client.get("/home")
+        assert rv.status_code == 302
 
+    def test_home_route_is_rendered_when_authenticated(self, client):
+        rv = client.post(
+            "/home",
+            data={"email": "secretary@club1.com"},
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+        assert rv.status_code == 200
 
-def test_home_route_is_rendered_when_authenticated(client):
-    rv = client.post(
-        "/home",
-        data={"email": "secretary@club1.com"},
-        headers={"Content-Type": "application/x-www-form-urlencoded"},
-    )
-    assert rv.status_code == 200
-
-
-def test_home_route_is_rendered_when_club_does_not_exist(client):
-    rv = client.post(
-        "/home",
-        data={"email": "unexisting@email.com"},
-        headers={"Content-Type": "application/x-www-form-urlencoded"},
-    )
-    assert rv.status_code == 302
+    def test_home_route_is_rendered_when_club_does_not_exist(self, client):
+        rv = client.post(
+            "/home",
+            data={"email": "unexisting@email.com"},
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+        assert rv.status_code == 302
 
 
 def test_book_route_redirects_when_unauthenticated(client):
